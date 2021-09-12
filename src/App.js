669291bde0569c876eeffb9519a2b43f6138289f
ay-sugiura <bun.cedar@gmail.com>
2021-09-12 18:59:48 +0900

    カードを複数並べてみた

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organism/user/UserCard";
 import { HeaderOnly } from "./components/templates/HeaderOnly";
 import { Link, BrowserRouter, Route } from "react-router-dom";
+import styled from "styled-components";
 
 const user = {
   name: "すいか食べたい子",
@@ -25,10 +26,21 @@ function App() {
         <br />
         <SearchInput />
         <br />
-        <UserCard user={user} />
+        <SContainer>
+          <UserCard user={user} />
+          <UserCard user={user} />
+          <UserCard user={user} />
+          <UserCard user={user} />
+          <UserCard user={user} />
+        </SContainer>
+        
       </HeaderOnly>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const SContainer = styled.div`
+
+`;
